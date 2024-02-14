d1cabe9cb6220f807ecc8e2df9a89cb962c45072
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-02-14 21:03:06 +0530

    Commit:6

---
+++ SectionTwo/script.js
@@ -0,0 +1,6 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!')
+
+// explicitly mentioning 'console.log' to access the developers console
+// output is displayed in developers console
+console.log(40 + 8 + 23 - 10)
