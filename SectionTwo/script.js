29ebe3ef433e5920988f5dee41f52fad9a4f59b7
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-02-15 21:00:31 +0530

    Commit:9

--- SectionTwo/script.js
+++ SectionTwo/script.js
@@ -19,7 +19,7 @@ console.log(firstName);
 // --------------------------------------------------------------------
 
 
-// variable name convention
+// variable name conventions
 // --------------------------------------------------------------------
 
 // allowed
@@ -67,5 +67,84 @@ let name = 'Jonas';
 // the color will be differently used for constants
 let PI = 3.1415
 let E = 2.71
+
+// --------------------------------------------------------------------
+
+
+// 7 Prmitive Data Types in JavaScript
+// JavaScript has Dynamic Typing
+// --------------------------------------------------------------------
+
+// 1. Number 
+// used for floating point number 
+let age = 23;
+console.log(age);
+
+// 2. String 
+// Used for Text
+let fName = "Jonas";
+console.log(fName);
+
+// 3. Boolean
+// used for taking decisions
+let fullAge = true;
+console.log(fullAge);
+console.log(false)
+
+// 4. Undefined
+// value taken by a variable that is not yet defined('empty value')
+let children;
+console.log(children);
+
+// 5. null
+// also means empty value
+let emptyValue = null;
+console.log(emptyValue);
+
+// 6. Symbol (ES2015)
+// value that is unique and cannot be changed
+// will do it later
+
+// 7. BigInt (ES2020)
+// larger integers that the 'Number' type can hold
+// will do it later
+
+// --------------------------------------------------------------------
+
+
+// operators
+// --------------------------------------------------------------------
+
+// typeof
+console.log(typeof true);
+console.log(typeof fullAge);
+console.log(typeof 23);
+console.log(typeof 'Jonas');
+console.log(typeof fName);
+
+// --------------------------------------------------------------------
+
+
+// re-assigning a variable
+// --------------------------------------------------------------------
+
+// 1. 
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+javascriptIsFun = 'YES';
+console.log(javascriptIsFun);
+
+// 2. 
+let year;
+console.log(year);  // value is 'undefined'
+console.log(typeof year);   // data type is 'undefined'
+
+year = 1991;
+console.log(year);  
+console.log(typeof year);
+
+// 3. 
+console.log(typeof null);   // should be 'undefined' but shows 'object' (it's a bug)
 
 // --------------------------------------------------------------------
