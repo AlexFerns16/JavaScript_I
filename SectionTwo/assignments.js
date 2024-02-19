37db1a71481d45015c6614c5fbd529eb8590ec0c
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-02-19 20:05:21 +0530

    Commit:13

--- SectionTwo/assignments.js
+++ SectionTwo/assignments.js
@@ -5,3 +5,59 @@ let population = ' 10';
 console.log(country);
 console.log(continent);
 console.log(population);
+
+
+
+// coding challenge 1 - part 1
+// --------------------------------------------------------------------
+
+/*
+
+Mark and John are trying to compare their BMI (Body Mass Index), which is
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg
+and height in meter).
+
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about
+whether Mark has a higher BMI than John.
+
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76
+m tall.
+
+*/
+
+// test data 1
+const massMark1 = 78;
+const heightMark1 = 1.69;
+const massJohn1 = 92;
+const heightJohn1 = 1.95;
+
+const BMIMark1 = massMark1 / heightMark1 ** 2;
+const BMIJohn1 = massJohn1 / (heightJohn1 * heightJohn1);
+console.log(BMIMark1, BMIJohn1);
+
+const markHigherBMI1 = BMIMark1 > BMIJohn1;
+console.log(markHigherBMI1);
+
+
+// test data 2
+const massMark2 = 95;
+const heightMark2 = 1.88;
+const massJohn2 = 85;
+const heightJohn2 = 1.76;
+
+const BMIMark2 = massMark2 / heightMark2 ** 2;
+const BMIJohn2 = massJohn2 / (heightJohn2 * heightJohn2);
+console.log(BMIMark2, BMIJohn2);
+
+const markHigherBMI2 = BMIMark2 > BMIJohn2;
+console.log(markHigherBMI2);
+
+// --------------------------------------------------------------------
