dc0d2dd20d345ad70b351c7777887fcdca18e33c
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-03-10 14:30:31 +0530

    commit:26 | functions

---
+++ SectionThree/script2.js
@@ -0,0 +1,38 @@
+
+// functions
+
+// --------------------------------------------------------------------
+
+// defining a function
+function logger() {
+    console.log('My name is Jonas');
+}
+
+// calling / running / invoking a function
+logger();
+logger();
+logger();
+
+
+//
+function fruitProcessor(apples, oranges) {
+    console.log(apples, oranges);
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
+}
+
+//
+console.log(fruitProcessor(5, 0));
+
+//
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+//
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+// built-in function
+Number('23');
+
+// --------------------------------------------------------------------
