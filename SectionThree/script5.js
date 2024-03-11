1bdd21070578f251d1e08743c5195745761be06f
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-03-11 06:27:19 +0530

    commit:29 | functions

---
+++ SectionThree/script5.js
@@ -0,0 +1,19 @@
+
+// functions calling other functions
+// --------------------------------------------------------------------
+
+function cutFruitPieces(fruit) {
+    return fruit * 4;
+}
+
+function fruitProcessor(apples, oranges) {
+    const applePieces = cutFruitPieces(apples);
+    const orangePieces = cutFruitPieces(oranges);
+
+    const juice = `Juice with ${applePieces} pieces of apples and ${orangePieces} pieces of oranges`;
+    return juice;
+}
+
+console.log(fruitProcessor(2, 3));
+
+// --------------------------------------------------------------------
