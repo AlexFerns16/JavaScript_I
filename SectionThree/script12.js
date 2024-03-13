faac871d5b48e39f6b96bf185fd3e3fcade161da
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-03-13 19:45:06 +0530

    commit:39 | for loop

--- SectionThree/script12.js
+++ SectionThree/script12.js
@@ -2,6 +2,28 @@
 // looping backwards and loops in loops
 // --------------------------------------------------------------------
 
+// 1. 
+const jonasArray = [
+    'Jonas',
+    'Schmedtmann',
+    2037 - 1991,
+    'teacher',
+    ['Michael', 'Peter', 'Steven'],
+    true
+]
 
+// 
+for (let i = jonasArray.length - 1; i >= 0; i--) {
+    console.log(jonasArray[i]);
+}
+
+// loop inside loop
+for (let exercise = 1; exercise < 4; exercise++) {
+    console.log(`----------- Starting exercise ${exercise}`);
+
+    for (let rep = 1; rep < 6; rep++) {
+        console.log(`Exercise ${exercise}: Lifting weight repetition ${rep}`);
+    }
+}
 
 // --------------------------------------------------------------------
