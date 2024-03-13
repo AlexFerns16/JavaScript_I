92a32b49192b14b38aa70499e6359b7ccdf6756c
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-03-13 20:20:38 +0530

    commit:40 | while loop

--- SectionThree/script13.js
+++ SectionThree/script13.js
@@ -1,7 +1,27 @@
 
 // the while loop
+// use this, whenever you need a loop without the counter
+// and/or, when you do not know before hand how many durations the loop will have
 // --------------------------------------------------------------------
 
+// 1. 
+let rep = 1;
+while (rep <= 10) {
+    console.log(`WHILE: Lifting weights repetition ${rep}`);
+    rep++;
+}
 
+
+// 2. 
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+
+    if (dice === 6) 
+        console.log('Loop is about to end...');
+}
 
 // --------------------------------------------------------------------
