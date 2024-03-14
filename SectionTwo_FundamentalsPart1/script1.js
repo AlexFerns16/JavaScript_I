dd7c900193b13cb50228d2fd950a3d2c5b4270d8
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-03-14 22:57:04 +0530

    commit:45 | prettier

--- SectionTwo_FundamentalsPart1/script1.js
+++ SectionTwo_FundamentalsPart1/script1.js
@@ -26,46 +26,57 @@ console.log(firstName);
 // variable name conventions
 // --------------------------------------------------------------------
 
+// PARTS
 // allowed
 let first;
 
+// PARTS
 // allowed
 let firstNamePerson;
 
+// PARTS
 // not allowed
 // let 3years;
 
+// PARTS
 // not allowed
 // let jonas&matilda;
 
+// PARTS
 // allowed
 let jonas_matilda;
 jonas_matilda = "JM";
 
+// PARTS
 // allowed
 // specific use case for object oriented programming
 // initial letter in 'caps'
 let Person = "Jonas";
 
+// PARTS
 // not allowed
 // 'new' is a reserved keyword in JS
 // let new = 27;
 
+// PARTS
 // not allowed
 // 'function' is a reserved keyword in JS
 // let function = 27;
 
+// PARTS
 // allowed
 // since the reserved keywords starts with '_' and '$'
 // only '_' and '$' are allowed as prefix to the variable name in JS
 let _function = 27;
 let $new = 27;
 
+// PARTS
 // 'name' is also a reserved keyword in JS,
 // but still allowed to be used
 // it is a good practice to not use it as a variable name
 let name = "Jonas";
 
+// PARTS
 // for constants that will never change
 // there is a convention of writing it all in uppercase
 // the color will be differently used for constants
@@ -150,55 +161,72 @@ console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
 
 // INNERSECTION
 // 4.
+
+// PARTS
 // arithmatic - addition
 const firstPerName = "Jonas";
 const lastPerName = "Schmedtmann";
 
+// PARTS
 // string concatenation
 console.log(firstPerName + " " + lastPerName);
 
 // INNERSECTION
 // 5.
+
+// PARTS
 // assignment
 let x = 10 + 5;
 console.log(x);
 
+// PARTS
 // x = x + 10
 x += 10;
 console.log(x);
 
+// PARTS
 // x = x * 4
 x *= 4;
 console.log(x);
 
+// PARTS
 // x = x + 1;
 x++;
 console.log(x);
 
+// PARTS
 // x = x - 1;
 x--;
 console.log(x);
 
 // INNERSECTION
 // 6.
+
+// PARTS
 // comparison
 // it is used to produce boolean values
 
+// PARTS
 // greater than
 console.log(ageJonas > ageSarah);
 
+// PARTS
 // less than
 console.log(ageJonas < ageSarah);
 
+// PARTS
 // greater than or equal to
 console.log(ageSarah >= 18);
 
+// PARTS
 // less than or equal to
 console.log(ageJonas <= 18);
 
+// PARTS
 // example 1
 const isFullAge = ageSarah >= 18;
 
+// PARTS
 // example 2
 console.log(now - 1991 > now - 2018);
 console.log(now - 1991 > now - 2018);
@@ -210,12 +238,13 @@ console.log(now - 1991 > now - 2018);
 // google: 'mdn operator precedence'
 // --------------------------------------------------------------------
 
-//
+// PARTS
 const nowYear = 2037;
 const ageOfJonas = now - 1991;
 const ageOfSarah = now - 2018;
 console.log(ageOfJonas, ageOfSarah);
 
+// PARTS
 // 'precedence' is priority of operators
 // operators with same precedence will execute on the basis of 'associativity'
 // the '-' operator has a higher precedence over '>' operator
@@ -227,16 +256,19 @@ console.log(ageOfJonas, ageOfSarah);
 // with the result of 'nowYear - 1991' being the 'left operand' and the result of 'nowYear - 2018' being the 'right operand'
 console.log(nowYear - 1991 > nowYear - 2018);
 
+// PARTS
 // arithmatic operators
 // 'left-to-right' precedence
 console.log(25 - 10 - 5);
 
+// PARTS
 // assignment operators
 // 'right-to-left'
 let a, b;
 a = b = 25 - 10 - 5;
 console.log(a, b);
 
+// PARTS
 // '()' has a higher precedence
 const averageAge = (ageJonas + ageSarah) / 2;
 console.log(ageJonas, ageSarah, averageAge);
@@ -249,18 +281,24 @@ console.log(ageJonas, ageSarah, averageAge);
 
 // INNERSECTION
 // 1.
+
+// PARTS
 let javascriptIsFun = true;
 console.log(javascriptIsFun);
 
+// PARTS
 javascriptIsFun = "YES";
 console.log(javascriptIsFun);
 
 // INNERSECTION
 // 2.
+
+// PARTS
 let year;
 console.log(year); // value is 'undefined'
 console.log(typeof year); // data type is 'undefined'
 
+// PARTS
 year = 1991;
 console.log(year);
 console.log(typeof year);
@@ -287,14 +325,18 @@ console.log(agePer);
 
 // INNERSECTION
 // 2.
+
+// PARTS
 // creates a variable that we cannot re-assign
 // a variable that cannot be mutated
 const birthYear = 1991;
 console.log(birthYear);
 
+// PARTS
 // Console: Uncaught TypeError: Assignment to constant variable.
 // birthYear = 1990;
 
+// PARTS
 // Console: Uncaught SyntaxError: Missing initializer in const declaration
 // missing initializer in 'const' declaration
 // when declaring 'const' the value needs to be initialized, unlike 'let'
@@ -302,9 +344,12 @@ console.log(birthYear);
 
 // INNERSECTION
 // 3.
+
+// PARTS
 // old way of defining the variables
 var job = "programmer";
 
+// PARTS
 // mutating 'job' variable
 job = "teacher";
 console.log(job);
@@ -397,11 +442,13 @@ console.log(temp3);
 // strings
 // --------------------------------------------------------------------
 
+// PARTS
 const firstPersonName = "Jonas";
 const jobFirPer = "teacher";
 const birthYearFirPer = 1991;
 const currentYear = 2037;
 
+// PARTS
 // '(currentYear - birthYearFirPer)' > outputs an integer
 // but when concatenated with a string, gets typecasted by itself to a string output
 const jonasOne =
@@ -414,6 +461,7 @@ const jonasOne =
   "!";
 console.log(jonasOne);
 
+// PARTS
 // ``(back-ticks) > used for template string / template literal
 const jonasTwo = `I'm ${firstPersonName}, a ${
   currentYear - birthYearFirPer
@@ -422,9 +470,11 @@ console.log(jonasTwo);
 const jonasThr = `I'm ${"Jonas"}, a ${46} years old ${"teacher"}!`;
 console.log(jonasThr);
 
+// PARTS
 // can be used for regular string as well
 console.log(`Just a regular string...`);
 
+// PARTS
 // string with multiple lines
 console.log(
   "String with \n\
@@ -432,6 +482,7 @@ multiple \n\
 lines"
 );
 
+// PARTS
 // string with multiple lines with 'back-ticks'
 console.log(`String with
 multiple
@@ -445,22 +496,29 @@ lines`);
 
 // INNERSECTION
 // 1.
+
+// PARTS
 const agePersonOne = 19;
 const isOldEnoughOne = agePersonOne >= 18;
 
+// PARTS
 if (isOldEnoughOne) {
   console.log("Sarah can apply for driving license 😊");
 }
 
+// PARTS
 if (agePersonOne >= 18) {
   console.log("Sarah can apply for driving license 😊");
 }
 
 // INNERSECTION
 // 2.
+
+// PARTS
 const agePersonTwo = 15;
 const isOldEnoughTwo = agePersonTwo >= 18;
 
+// PARTS
 if (isOldEnoughTwo) {
   console.log("Sarah can apply for driving license 😊");
 } else {
@@ -470,6 +528,7 @@ if (isOldEnoughTwo) {
   );
 }
 
+// PARTS
 if (agePersonTwo >= 18) {
   console.log("Sarah can apply for driving license 😊");
 } else {
