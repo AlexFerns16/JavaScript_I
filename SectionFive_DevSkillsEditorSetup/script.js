eb1881d95697bcdcd3e9903def1325d0c354f693
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-03-14 13:35:26 +0530

    commit:43 | prettier and vsc

---
+++ SectionFive_DevSkillsEditorSetup/script.js
@@ -0,0 +1,14 @@
+// Remember, we're gonna use strict mode in all scripts now!
+"use strict";
+
+const xyzVariable = 23;
+if (xyzVariable === 20) console.log(23);
+
+const calcAge = (birthYear) => 2037 - birthYear;
+console.log(xyzVariable);
+
+// IMP
+// BUG
+// FIXME
+// TODO
+// TEST
