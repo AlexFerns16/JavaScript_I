e9cf18412a34ba71e194ac9e886e7cc9ab28e85b
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-04-22 18:30:54 +0530

    commit:52 | DOM & Events

--- SectionSeven_DOM&Events/script2.js
+++ SectionSeven_DOM&Events/script2.js
@@ -1,5 +1,6 @@
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let highScore = 0;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -16,21 +17,16 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
 
-    // when the guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '😎 Too High!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '😌 You lost the game!';
-      document.querySelector('.score').textContent = 0;
+    if (score > highScore) {
+      highScore = score;
+      document.querySelector('.highscore').textContent = highScore;
     }
 
-    // when the guess is too low
-  } else if (guess < secretNumber) {
+    // when the guess is wrong
+  } else if (guess !== secretNumber) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '😎 Too Low!';
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? '😎 Too High!' : '😎 Too Low!';
       score--;
       document.querySelector('.score').textContent = score;
     } else {
