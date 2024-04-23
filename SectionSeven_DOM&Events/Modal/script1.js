2b2621d979ca1d275acd3cfa4437b014daf91648
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-04-24 01:27:30 +0530

    commit:60 | DOM & Events | Modal

--- SectionSeven_DOM&Events/Modal/script1.js
+++ SectionSeven_DOM&Events/Modal/script1.js
@@ -6,19 +6,21 @@ const btnCloseModal = document.querySelector('.close-modal');
 const btnShowModal = document.querySelectorAll('.show-modal');
 console.log(btnShowModal);
 
-for (let i = 0; i < btnShowModal.length; i++) {
-  console.log(btnShowModal[i].textContent);
-  btnShowModal[i].addEventListener('click', function () {
-    console.log('Button Clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
+const openModal = function () {
+  console.log('Button Clicked');
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+for (let i = 0; i < btnShowModal.length; i++) {
+  console.log(btnShowModal[i].textContent);
+  btnShowModal[i].addEventListener('click', openModal);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
