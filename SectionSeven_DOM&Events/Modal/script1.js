d323983088fcb5217f4e5286275803c4a485f4f0
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-04-24 02:02:04 +0530

    commit:61 | DOM & Events | Modal

--- SectionSeven_DOM&Events/Modal/script1.js
+++ SectionSeven_DOM&Events/Modal/script1.js
@@ -24,3 +24,12 @@ for (let i = 0; i < btnShowModal.length; i++) {
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  console.log('A key was pressed');
+  console.log(e.key);
+
+  if (e.key === 'Escape') {
+    console.log('Escape was pressed');
+  }
+});
