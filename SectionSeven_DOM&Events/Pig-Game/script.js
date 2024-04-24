167d6e3bd5a83f4edd9f32c62feecac10472d584
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-04-24 18:51:51 +0530

    commit:67 | DOM & Events | Pig Game

--- SectionSeven_DOM&Events/Pig-Game/script.js
+++ SectionSeven_DOM&Events/Pig-Game/script.js
@@ -2,7 +2,7 @@
 
 // selecting elements
 const score0El = document.querySelector('#score--0');
-const score1El = document.getElementById('score--1');
+const score1El = document.querySelector('#score--1');
 const current0El = document.getElementById('current--0');
 const current1El = document.getElementById('current--1');
 const diceEl = document.querySelector('.dice');
