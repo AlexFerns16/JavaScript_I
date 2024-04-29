c85fd9331efb94dbe5eea2661d05aeaab5d8d36b
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-04-29 13:09:50 +0530

    commit:77 | Scoping in practice

---
+++ SectionEight_JavaScriptBehindTheScenes/script1.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// global scope execution context
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+  console.log(firstName);
+  return age;
+}
+
+// global variable
+const firstName = 'Jonas';
+
+// call to function calcAge
+calcAge(1991);
