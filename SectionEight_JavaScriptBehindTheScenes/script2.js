d7ccf68b0c1b6627b795a465feb49469d5a99920
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-05-02 19:00:31 +0530

    commit:82 | Hoisting and TDZ in Practice

--- SectionEight_JavaScriptBehindTheScenes/script2.js
+++ SectionEight_JavaScriptBehindTheScenes/script2.js
@@ -1,26 +1,32 @@
 'use strict';
 
 /* variables */
+/* ------------------------------------------------------------------- */
 
 console.log(me);
 // console.log(job);
 // console.log(year);
 
 // 'TDZ' > Temporal Dead Zone
+// 'TDZ' is the zone before the declaration of the variable
+// 'me' is called before declaration
 // variables declared with 'var' are hoisted to the value of 'undefined' in 'TDZ'
 var me = 'Jonas';
 
 // variables declared with 'let' are not hoisted
 // cannot be accessed in 'TDZ' > Temporal Dead Zone
 // 'TDZ' is the zone before the variable is declared
+// 'job' is called before declaration
 let job = 'teacher';
 
 // variables declared with 'const' are not hoisted
 // cannot be accessed in 'TDZ' > Temporal Dead Zone
 // 'TDZ' is the zone before the variable is declared
+// 'year' is called before declaration
 const year = 1991;
 
 /* functions */
+/* ------------------------------------------------------------------- */
 
 console.log(addDecl(2, 3));
 // console.log(addExpr(2, 3));
@@ -56,3 +62,18 @@ var addExpr = function (a, b) {
 // function name is viewed as 'undefined(a, b)', which does not exists
 // therefore, it cannot be called before defining
 var addArrow = (a, b) => a + b;
+
+// example
+// ----------------------------------------------------------------------
+
+// even when 'numProducts' is defined to '10'
+// using it before defining it would yield the value as 'undefined'
+// which would result in boolean 'false'
+// '!false > true' which would execute the 'deleteShoppingCart()'
+// hence this would result in a big mistake
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+var numProducts = 10;
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
