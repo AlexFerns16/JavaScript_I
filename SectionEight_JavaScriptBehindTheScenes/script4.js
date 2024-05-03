cfc9cc6ef2633d614b84d142f2ad3b7c946f6737
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-05-03 13:23:34 +0530

    commit:89 | Regular Functions vs Arrow Functions

--- SectionEight_JavaScriptBehindTheScenes/script4.js
+++ SectionEight_JavaScriptBehindTheScenes/script4.js
@@ -14,6 +14,19 @@ const jonas = {
   calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
+
+    const isMillenial = function () {
+      console.log(this);
+      console.log(this.year >= 1981 && this.year <= 1996);
+    };
+
+    // we know that, a 'function' that is attached to an 'object' is called a 'method'
+    // and the method is called using the object name
+    // the 'this' keyword inside a method thus refers to the same object
+    // 'isMillenial()' is just a regular function call inside of a method
+    // hence as the rule states, inside a regular function call the keyword 'this' must be 'undefined'
+    // since the function 'isMillenial()' is not attached to the 'object'
+    isMillenial();
   },
   // arrow function doesn't get's it's own 'this' keyword
   // but it takes it from the parent scope
@@ -21,22 +34,23 @@ const jonas = {
   // const jonas = { ... }, is not a code block, so it is not the scope
   // it is an object literal
   // this is why we shouldn't use the arrow function as a method
-  //   greet: () => {
-  //     // 'this' > 'window'
-  //     console.log(this);
-  //     console.log(`Hey, ${this.firstName}`);
-  //   },
-  greet: function () {
-    // 'this' > 'jonas' object
+  greet: () => {
+    // 'this' > 'window'
     console.log(this);
     console.log(`Hey, ${this.firstName}`);
   },
+  //   greet: function () {
+  //     // 'this' > 'jonas' object
+  //     console.log(this);
+  //     console.log(`Hey, ${this.firstName}`);
+  //   },
 };
 
 // results in 'Hey, undefined'
 jonas.greet();
+jonas.calcAge();
 
 // the 'this' keyword it refers to is in the global scope
 // 'this' > 'window'
-console.log(this.firstName);
-console.log(this);
+// console.log(this.firstName);
+// console.log(this);
