4c6c7d06180c08fbc5b6581b6e175783ce0dd198
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-05-09 12:39:08 +0530

    commit:94 | Destructing Arrays

---
+++ SectionNine_DataStructuresModernOperatorsStrings/script.js
@@ -0,0 +1,59 @@
+'use strict';
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+// Data needed for first part of the section
+const restaurant = {
+  name: 'Classico Italiano',
+  location: 'Via Angelo Tavanti 23, Firenze, Italy',
+  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
+  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
+  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+
+  openingHours: {
+    thu: {
+      open: 12,
+      close: 22,
+    },
+    fri: {
+      open: 11,
+      close: 23,
+    },
+    sat: {
+      open: 0, // Open 24 hours
+      close: 24,
+    },
+  },
+};
+
+// destructuring an array
+// unpacking
+
+//
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+console.log(a, b, c);
+console.log(arr);
+
+//
+const [x, y, z] = arr;
+console.log(x, y, z);
+console.log(arr);
+
+//
+let [first1, second1] = restaurant.categories;
+console.log(first1, second1);
+
+//
+let [first2, , third2] = restaurant.categories;
+console.log(first2, third2);
+
+// swapping
+const temp = first2;
+first2 = third2;
+third2 = temp;
+console.log(first2, third2);
