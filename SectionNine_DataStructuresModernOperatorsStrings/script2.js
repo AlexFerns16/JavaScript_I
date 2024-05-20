e0c917238bfc399e3812c6c8204b0bc67549b52b
AlexF777 <48488198+AlexF777@users.noreply.github.com>
2024-05-20 18:33:30 +0530

    commit:102 | Destructing Objects

--- SectionNine_DataStructuresModernOperatorsStrings/script2.js
+++ SectionNine_DataStructuresModernOperatorsStrings/script2.js
@@ -16,7 +16,12 @@ const restaurant = {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivery: function ({ starterIndex, mainIndex, time, address }) {
+  orderDelivery: function ({
+    starterIndex = 1,
+    mainIndex = 0,
+    time = '20:00',
+    address,
+  }) {
     console.log(
       `Order Received for ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
@@ -38,11 +43,18 @@ const restaurant = {
   },
 };
 
+//
 restaurant.orderDelivery({
   time: '22:30',
   address: 'Via del Sole, 21',
   mainIndex: 2,
   starterIndex: 2,
+});
+
+//
+restaurant.orderDelivery({
+  address: 'Via del Sole, 22',
+  starterIndex: 1,
 });
 
 // destructuring objects
